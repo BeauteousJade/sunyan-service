9562f9a0c73160a7adce02c18d71ad2313b13bf3
BeauteousJade <pby2196995023@163.com>
2019-04-12 16:39:27 +0800

    项目建立完成

---
+++ src/main/java/com/cly/sunyan/SunYanApplication.java
@@ -0,0 +1,13 @@
+package com.cly.sunyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunYanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunYanApplication.class, args);
+    }
+
+}
